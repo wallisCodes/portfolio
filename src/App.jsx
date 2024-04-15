2794c97afc7ead78aec983d3a815fa2b0bb4f98f
wallisCodes <jwallis152@gmail.com>
2024-04-15 18:15:27 +0100

    Added tailwind navbar and custom theme colours

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Nav from '../components/Nav'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-white text-center font-bold bg-rg-primary">Hi, I'm Wallis.</h1>
-      <h2 className="text-xl text-white text-center bg-rg-secondary">Welcome to my portfolio!</h2>
+      <Nav />
+      <h1 className="bg-primary-light text-3xl text-white text-center font-bold">Hi, I'm Wallis.</h1>
+      <h1 className="bg-primary text-3xl text-alt-light text-center font-bold">Hi, I'm Wallis.</h1>
+      <h1 className="bg-alt-primary text-3xl text-white text-center font-bold">Hi, I'm Wallis.</h1>
+      <h1 className="bg-primary-dark text-3xl text-white text-center font-bold">Welcome to my portfolio!</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        "rg-primary": "#c85a19",
-        "rg-secondary": "#006039",
-        "wmb-primary": "#006633",
-        "wmb-secondary": "#4c2178",
-        "uso-primary": "#00228d",
-        "uso-secondary": "#fc7d00",
-        "ao-primary": "#1b8cca",
+        "primary": "#026773",
+        "primary-light": "#3CA6A6",
+        "primary-dark": "#012E40",
+        "alt-primary": "#024959",
+        "alt-light": "#F2E3D5", 
       },
     },
   },
