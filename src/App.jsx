1e5762261e2b47ab294492dcc0b4eb3a740f5cc1
wallisCodes <jwallis152@gmail.com>
2024-04-17 14:36:08 +0100

    Styled navbar and added links to personal github and linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <Nav />
       <h1 className="bg-primary-light text-3xl text-white text-center font-bold">Hi, I'm Wallis.</h1>
-      <h1 className="bg-primary text-3xl text-alt-light text-center font-bold">Hi, I'm Wallis.</h1>
+      <h1 className="bg-primary text-3xl text-white text-center font-bold">Hi, I'm Wallis.</h1>
       <h1 className="bg-alt-primary text-3xl text-white text-center font-bold">Hi, I'm Wallis.</h1>
       <h1 className="bg-primary-dark text-3xl text-white text-center font-bold">Welcome to my portfolio!</h1>
     </>
